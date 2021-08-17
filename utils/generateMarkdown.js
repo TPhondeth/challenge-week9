31c35040b9f583f94ff465a93124990f510d7ca8
Tony Phondeth <phondeth@gmail.com>
2021-08-16 23:09:46 -0400

    updated generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,43 +1,12 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (answers.license = true) {
-    return '(https://img.shields.io/badge/license-${answers.license}-brightgreen)'
-  } else {
-    return '';
-  }
-}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (answers.license = true) {
-    return '(https://choosealicense.com/licenses/${answers.license}/)'
-  } else {
-    return '';
-  }
-};
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (answers.license = true) {
-    return (license)
-  } else {
-    return '';
-  }
-};
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(answers) {
+// Function to generate markdown for README
+function generateMarkdown(data) {
   return `
-  # Title
-    <h1>${answers.title}</h1>
+  # ${data.title}
 
-  (https://img.shields.io/badge/license-${answers.license}-brightgreen)\n
+  (https://img.shields.io/badge/license-${data.license}-brightgreen)\n
 
   ## Description
-    ${answers.description}
+    ${data.description}
 
   ## Table of Contents
   - [Description](#description)
@@ -49,24 +18,23 @@ function generateMarkdown(answers) {
   - [Questions](#questions)
 
   ## Installation
-    ${answers.installation}
+    ${data.installation}
 
   ## Usage 
-    ${answers.usage}
+    ${data.usage}
+
+  ## Tests
+    ${data.tests}
 
   ## License
-    This application is covered under the ${answers.license} license. (https://choosealicense.com/licenses/${answers.license}/)
+    This application is covered under the ${data.license} license. (https://choosealicense.com/licenses/${data.license}/)
 
   ## Contributing
-    ${answers.contributing}
-  
-  ## Tests
-    ${answers.tests}
+    ${data.contributing}
 
   ## Questions
-    ${answers.github} (https://github.com/${answers.github})
-    Have additional questions? Please email: ${answers.email}\n
-`;
+    Github: ${data.github} (https://github.com/${data.github})
+    Have additional questions? Please email: ${data.email}\n`;
 }
 
 module.exports = generateMarkdown;
